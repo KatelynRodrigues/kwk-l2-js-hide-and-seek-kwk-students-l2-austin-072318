f12482ce806e57059741c441a3922e4616bb8d68
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-27 16:16:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //Code your solution here
-function getFirstSelector(selector){
-  return document.querySelector(selector)
-}
+// function getFirstSelector(selector){
+//   return document.querySelector(selector)
+// }
 
-function nestedTarget(){
-  return document.getElementById("nested")
-}
+// function nestedTarget(){
+//   return document.getElementById("nested")
+// }
 
-// function increaseRankBy(n){
-//   var ranks = document.querySelectorAll("ul.ranked-list li")
-//   for (i=0; i<ranks.length; i++){
-//     ranks[i].innerHTML = (i + 1).toString()
-//   }
-}
+// // function increaseRankBy(n){
+// //   var ranks = document.querySelectorAll("ul.ranked-list li")
+// //   for (i=0; i<ranks.length; i++){
+// //     ranks[i].innerHTML = (i + 1).toString()
+// //   }
+// }
