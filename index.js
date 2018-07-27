6fbf63b813985b76e91a0cb033a1719482d3facb
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-27 16:24:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function nestedTarget(){
   return document.getElementById("nested")
 }
 
-function increaseRankBy(){
+function increaseRankBy(n){
   var ranks = document.querySelectorAll("ul.ranked-list li")
   for (i=0; i<ranks.length; i++){
-    ranks[i].innerHTML = (i + 1).toString()
+    ranks[i].innerHTML = (i + n).toString()
   }
 }
